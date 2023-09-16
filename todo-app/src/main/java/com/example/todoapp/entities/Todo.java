e6b5b36dacd3ff5da5daf295b31df39004605494
Stevan Stankovic <stevanstankovic603@gmail.com>
2023-09-16 21:06:50 +0200

    Create model repository and done flyway migrations

---
+++ todo-app/src/main/java/com/example/todoapp/entities/Todo.java
@@ -0,0 +1,27 @@
+package com.example.todoapp.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "todo")
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "completed")
+    private Boolean completed;
+
+}
